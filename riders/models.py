3ad50f87cf1b02ee1df845147e1c2c641a363296
Christian Watson <chris23w@gmail.com>
2019-06-14 16:15:30 -0700

    added models, index view

--- riders/models.py
+++ riders/models.py
@@ -1,3 +1,64 @@
 from django.db import models
+from django.urls import reverse
+from datetime import date
+from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
+
+# Created models
+
+
+# pretty self explanatory
+class Location(models.Model):
+    place = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
+
+    # override __str__ method
+    def __str__(self):
+        return self.name
+
+
+class Car(models.Model):
+    make = models.CharField(max_length=50)
+    model = models.CharField(max_length=50)
+    color = models.CharField(max_length=50)
+    maxOccupancy = models.IntegerField(
+        default=4,
+        validators=[MinValueValidator(2), MaxValueValidator(10)]
+    )
+
+    # override __str__ method
+    def __str__(self):
+        return f'A {self.color} {self.make} {self.model} with a max occupancy of {self.maxOccupancy}'
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    # don't need username, already on built in User model
+    # I'm thinking picture is just a link to imgur or something where we host the images
+    picture = models.CharField(max_length=100)
+    employer = models.CharField(max_length=50)
+    isDriver = models.BooleanField(default=False)
+
+    # override __str__ method
+    def __str__(self):
+        u = User.objects.get(pk=self.user)
+        return f'{u.username}'
+
+
+class Route(models.Model):
+    departureLocation = models.ForeignKey(Location, on_delete=models.CASCADE, related_name='departure')
+    arrivalLocation = models.ForeignKey(Location, on_delete=models.CASCADE, related_name='arrival')
+    users = models.ManyToManyField(Profile)
+    name = models.CharField(max_length=100)
+    # not sure about departureTime, since this is gonna be the same
+    # time for 5 days a week, seems weird to use actual time
+    # units since they snapshot a day, unless there's some
+    # different way to use em
+    departureTime = models.IntegerField(
+        validators=[MaxValueValidator(12), MinValueValidator(1)]
+    )
+
+    # override __str__ method
+    def __str__(self):
+        return f'A route that leaves from {self.departureLocation} at the hour of {self.departureTime} to {self.arrivalLocation}'
+
