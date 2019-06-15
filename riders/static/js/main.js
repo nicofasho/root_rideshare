647ccc3441942b574c8502fe7871dc5099b7cdea
alison-codes <alisonnorris314@gmail.com>
2019-06-15 10:32:14 -0700

    Add sidenav for mobile viewing

---
+++ riders/static/js/main.js
@@ -0,0 +1,6 @@
+// This is the function that hides the car animation.
+// In deployment, we'll likely want the time to be between 3000 and 60000 milleseconds
+setTimeout(function () {
+    $('.animation').fadeOut('fast');
+}, 1000);
+
