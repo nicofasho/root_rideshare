1cd9f205fe2802e3fd345d1910b858b6c984dba3
Akhil Neti <akhilnneti@gmail.com>
2019-06-17 17:23:18 -0700

    Update user to route match function

--- riders/views.py
+++ riders/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .models import Route, Location
+from .models import Route, Location, Profile
 
 # Auth
 from django.contrib.auth.forms import UserCreationForm
@@ -19,37 +19,12 @@ def home(request):
     return render(request, 'home.html')
 
 
+@login_required
 def riders_index(request):
-    routes = Route.objects.all()
+    curr_profile = Profile.objects.get(user=request.user)
+    routes = curr_profile.route_set.all()
     return render(request, 'riders/index.html', {'routes': routes})
 
-def route_query_AM(request):
-    # add default values in .GET()
-    potential_routes = Route.objects.filter(
-        departureTime=request.GET['arrivalTime']
-        ).filter(
-        departureLocation=request.GET['departure']
-        ).filter(
-        arrivalLocation=request.GET['destination']
-        )
-    return render(request, 'riders/index.html', {'routes': potential_routes})
-    # for route_AM in potential_routes:
-    #     if route_AM.users.count() < route_AM.car.maxOccupancy:
-    #         return render(request, 'riders/index.html', {'route_AM': route_AM})
-
-def route_query_PM(request):
-    # add default values in .GET()
-    potential_routes = Route.objects.filter(
-        departureTime=request.GET['arrivalTime']
-        ).filter(
-        departureLocation=request.GET['departure']
-        ).filter(
-        arrivalLocation=request.GET['destination']
-        )
-    return render(request, 'riders/index.html', {'routes': potential_routes})
-    # for route_PM in potential_routes:
-    #     if route_PM.users.count() < route_PM.car.maxOccupancy:
-    #         return render(request, 'riders/index.html', {'routes_PM': route_PM})
 
 def signup(request):
     error_message = ''
@@ -75,20 +50,46 @@ def signup(request):
 #         form.instance.user = self.request.user
 #         return super().form_valid(form)
 
+
+@login_required
 def route_search(request):
     locations = Location.objects.all()
     return render(request, 'riders/search.html', {'locations': locations})
 
+
+@login_required
 def route_query(request):
-    pass
+    # TO DO: add default values in .GET() [x]
+    # TO DO: add range to arrival time from departure time [x]
+    potential_routes = Route.objects.filter(
+        departureTime=request.GET['arrival']
+        ).filter(
+        departureLocation=request.GET['departure']
+        ).filter(
+        arrivalLocation=request.GET['destination']
+        )
+    # TO DO: can add similar functionality to the model instead or utilize F expressions
+    # TO DO: Models update later to "profiles"
+    # TO DO: logic to selecting carpool with fewest occupied seats, retrieve only on employer
+    curr_profile = Profile.objects.get(user=request.user)
+    for route in potential_routes:
+        if route.users.count() < route.car.maxOccupancy:
+            route.users.add(curr_profile)
+            break
+    return redirect('riders_index')
+
+# TO DO: Add a remove from route function
 
 class RouteDetail(LoginRequiredMixin, DetailView):
     model = Route
+
 
 class RouteUpdate(LoginRequiredMixin, UpdateView):
     model = Route
     fields = ['name', 'departureLocation', 'departureTime', 'arrivalLocation']
 
+
 class RouteDelete(LoginRequiredMixin, DeleteView):
     model = Route
     success_url = '/riders/'
+
